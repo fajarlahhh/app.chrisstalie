002c7f4c40aa62d28966193fffed7aaa4b69212b
Andi Fajar Nugraha <andifajarlah@gmail.com>
2026-04-03 17:14:10 +0800

    fix: refine harga_beli_tertinggi calculation in BarangClass to consider only the top three stokMasuk entries for improved accuracy

--- app/Class/BarangClass.php
+++ app/Class/BarangClass.php
@@ -43,7 +43,7 @@ class BarangClass
                 'barang_id' => $q['barang_id'],
                 'biaya' => $q['harga_jual'],
                 'harga' => $q['harga_jual'],
-                'harga_beli_tertinggi' => $q->stokMasuk->max(fn($r) => $r->harga_beli * $r->rasio_dari_terkecil) / $q->rasio_dari_terkecil,
+                'harga_beli_tertinggi' => $q->stokMasuk->take(3)->max(fn($r) => $r->harga_beli / $r->rasio_dari_terkecil) * $q->rasio_dari_terkecil,
                 'rasio_dari_terkecil' => $q['rasio_dari_terkecil'],
                 'satuan' => $q['barang_satuan_nama'],
                 'persediaan' => $q['persediaan'],
