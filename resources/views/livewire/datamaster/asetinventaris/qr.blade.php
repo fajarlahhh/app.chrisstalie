1b1ead42cc86f89859e735ae6128338c4c8f2ac3
Andi Fajar Nugraha <andifajarlah@gmail.com>
2026-04-07 08:20:33 +0800

    Hapus Archive

--- resources/views/livewire/datamaster/asetinventaris/qr.blade.php
+++ resources/views/livewire/datamaster/asetinventaris/qr.blade.php
@@ -1,7 +1,7 @@
 <table class="table table-borderless">
     <tr>
         <td class="p-0 text-end">
-            {!! QrCode::size(100)->generate($data->nomor) !!}
+            {!! \App\Helpers\QrCode::size(100)->generate($data->nomor) !!}
         </td>
         <td class="align-middle p-0">
             {{ $data->nomor }}
